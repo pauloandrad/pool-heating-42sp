eb8f3825d9a92874c3bd7593f79ca264bdaa4ff2
paulo andrade <pauloh8755@outlook.com.br>
2026-01-11 14:54:50 -0300

    fix: base conversion

--- include/C04_base_conversion.h
+++ include/C04_base_conversion.h
@@ -1,3 +1,5 @@
 #pragma once
 
 int ft_atoi(char *str);
+
+void ft_putnbr_base(int nbr, char *base);
--- src/C04_base_conversion.c
+++ src/C04_base_conversion.c
@@ -1,5 +1,6 @@
 #include "../include/C04_base_conversion.h"
 
+#include <unistd.h>
 
 int ft_atoi(char *str) {
     int i;
@@ -23,4 +24,52 @@ int ft_atoi(char *str) {
     }
 
     return sign * num;
+}
+
+int ft_get_base_len(char *base) {
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+
+    while (base[i] != '\0') {
+        if (base[i] == '-' || base[i] == '+' || (base[i] >= 9 && base[i] <= 13) || base[i] == ' ') return 0;
+
+        while (base[j] != '\0') {
+            if (i != j && base[i] == base[j]) return 0;
+            j++;
+        }
+        j = i + 1;
+        i++;
+    }
+
+    if (i <= 1) return 0;
+
+    return i;
+}
+
+void ft_calc_base(long nbr, char *base, int base_len) {
+    if (nbr >= base_len) {
+        ft_calc_base(nbr / base_len, base, base_len);
+    }
+
+    write(1, &base[nbr % base_len], 1);
+}
+
+void ft_putnbr_base(int nbr, char *base) {
+    int base_len;
+    long positive_n;
+
+    base_len = ft_get_base_len(base);
+
+    if (base_len == 0) return;
+    if (nbr < 0) {
+        positive_n = -nbr;
+        write(1, "-", 1);
+    } else {
+        positive_n = nbr;
+    }
+
+    ft_calc_base(positive_n, base, base_len);
 }
